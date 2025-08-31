ab2298b288654e7954c5b5837a4d8e8de64dd0f5
Theophilus Malatji <theo.malatji@gmail,com>
2025-08-31 21:07:00 +0200

    Task 2

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,6 @@
-from django.contrib import admin
 from . models import Book
+from django.contrib import admin
+
 # Register your models here.
 @admin.register(Book)
 class Bookadmin(admin.ModelAdmin):
