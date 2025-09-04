50a9b5f75f7f09099e81acef5a045c2001410ee1
TheophilusMalatji <theo.malatji@gmail.com>
2025-09-04 22:39:31 +0200

    Week 10 task 1

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from .import views
+from .views import list_books
+from .views import LibraryView
+
+
 
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
