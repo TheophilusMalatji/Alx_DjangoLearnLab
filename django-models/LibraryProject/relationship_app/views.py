bb733ea73728c6c759e44d37ad96fbac42d6e9bb
TheophilusMalatji <theo.malatji@gmail.com>
2025-09-04 22:30:44 +0200

    Week 10 task 0

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import *
 
 # Create your views here.
+
+def list_books(request):
+    books = Book.objects.all()
+    context = {list_books:books}
+    return render(request,'books/list_books.html', context )
+class LibraryView(ListView):
+    model = Library
+    context_object_name = 'library'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)        
+        context['books'] = self.object.books.all()
+        return context
