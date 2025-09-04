7445622b3a025ba8ae2a78ea57f254d8a3358ea6
TheophilusMalatji <theo.malatji@gmail.com>
2025-09-04 23:49:22 +0200

    Task 3

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -32,21 +32,21 @@ class SignUpView(UserCreationForm):
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
 
-def is_admin(user):   
+def Admin(user):   
     return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
 
-def is_librarian(user):    
+def Librarian(user):    
     return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
 
-def is_member(user):    
+def Member(user):    
     return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
 
-@user_passes_test(is_admin)
+@user_passes_test(Admin)
 def admin_view(request):
     return render(request, 'relationship_app/admin_view.html')
-@user_passes_test(is_member)
+@user_passes_test(Member)
 def admin_view(request):
     return render(request, 'relationship_app/member_view.html')
-@user_passes_test(is_librarian)
+@user_passes_test(Librarian)
 def admin_view(request):
     return render(request, 'relationship_app/librarian_view.html')
