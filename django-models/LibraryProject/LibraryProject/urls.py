426c3a6888d5e5d44b4174432697d567e09ed3ee
TheophilusMalatji <theo.malatji@gmail.com>
2025-09-04 23:21:59 +0200

    Templates

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -24,4 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('books/', views.list_books, name='list_books'),
     path('libraries/', LibraryView.as_view(), name='library_detail'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relogout'),name="logout")
 ]
