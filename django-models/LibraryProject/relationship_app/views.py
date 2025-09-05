4e307571a143dbf0194684692757c3cbb27f4ea1
TheophilusMalatji <theo.malatji@gmail.com>
2025-09-05 12:57:40 +0200

    Task 3

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -54,6 +54,10 @@ def is_admin(user):
 @user_passes_test(is_admin)
 def admin_view(request):
     return render(request, 'relationship_app/admin_view.html')
+@login_required
+@user_passes_test(is_admin)
+def Admin(request):
+    return render(request, 'relationship_app/admin_view.html')
 
 def is_librarian(user):
     return user.userprofile.role == 'Librarian'
