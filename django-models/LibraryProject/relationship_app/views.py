fc1675cf1fa4309fe770b345a3704219dcb5e0cb
TheophilusMalatji <theo.malatji@gmail.com>
2025-09-05 17:41:50 +0200

    Task 3

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -59,7 +59,7 @@ def is_member(user):
 
 @login_required
 @user_passes_test(is_admin, login_url="relationship_app:login")
-def AdminView(request):
+def admin_view(request):
     return render(request, 'relationship_app/admin_view.html')
 
 @login_required
