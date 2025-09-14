311de559fce28556427e6504c306ddeb12f64fa6
TheophilusMalatji <theo.malatji@gmail.com>
2025-09-14 21:57:16 +0200

    Task 0 Bookshelf

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,7 @@
 from .models import Book
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
 # Register your models here.
 @admin.register(Book)
@@ -7,3 +9,13 @@ class Bookadmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')    
     list_filter = ('author', 'publication_year')
     search_fields = ('title', 'author')
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['username', 'email', 'date_of_birth', 'profile_photo', 'is_staff']
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,4 +1,18 @@
 from django.db import models
+from django.db import models
+from django.contrib.auth.models import User,AbstractUser
+from .managers import CustomUserManager
+from django.conf import settings
+# Create your models here.
+# class UserProfile(models.Model):
+
+class CustomUser(AbstractUser):
+
+    date_of_birth = models.DateField()
+    profile_photo = models.ImageField()
+    objects = CustomUserManager()
+    def __str__(self):
+        return self.username
 
 # Create your models here.
 class Book(models.Model):
