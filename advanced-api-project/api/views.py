64a11fe3645904a7b031ae8df75ddd9397fdbcef
TheophilusMalatji <theo.malatji@gmail.com>
2025-09-28 18:42:21 +0200

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
 from serializers import *
-from rest_framework.permissions import BasePermission, IsAuthenticated
+from rest_framework.permissions import BasePermission, IsAuthenticated, IsAuthenticatedOrReadOnly
 
 
 class IsAdminUser(BasePermission):
