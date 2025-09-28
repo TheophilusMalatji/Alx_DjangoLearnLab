2206331b5458d490ee73b1beed08546d2dc5f794
TheophilusMalatji <theo.malatji@gmail.com>
2025-09-28 18:29:38 +0200

    Week 13 Task 1

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from rest_framework import generics
+from serializers import *
+from rest_framework.permissions import BasePermission, IsAuthenticated
+
+
+class IsAdminUser(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in ['DELETE', 'POST', 'PUT','PATCH']:
+            return True
+        return request.user.is_admin
+
 
 # Create your views here.
+class BookCreateView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAdminUser]
+
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookDetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookUpdateView(generics.ListUpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAdminUser]
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAdminUser]
