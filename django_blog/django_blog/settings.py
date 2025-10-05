a44264757cfa0169e30ee06b95d54161b279f429
TheophilusMalatji <theo.malatji@gmail.com>
2025-10-05 18:09:41 +0200

    correction settings path to read blog/static

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "blog/static",
 ]
 
 TEMPLATES = [
@@ -129,6 +129,14 @@ TEMPLATES = [
             'BACKEND': 'django.template.backends.django.DjangoTemplates',
             'DIRS': [],
             'APP_DIRS': True,
+             'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request', 
+                'django.contrib.auth.context_processors.auth',   
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
            
         },
     ]
